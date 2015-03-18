cd74901c56505579a1517cd4f0fdd260cd8c4d5e
Chan <Chan@Lightning_pc>
2015-03-18 10:44:43 +0800

    完成实例模板，Android开发时，实现侧拉菜单可以直接用！

--- src/sduwh/cp/drawerlayout/MainActivity.java
+++ src/sduwh/cp/drawerlayout/MainActivity.java
@@ -1,36 +1,136 @@
 package sduwh.cp.drawerlayout;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+public class MainActivity extends Activity implements OnItemClickListener {
 
-public class MainActivity extends Activity {
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerList;
+	private ArrayList<String> menulists;
+	private ArrayAdapter<String> adapter;
+	private ActionBarDrawerToggle mDrawerToggle;
+	private String mTitle;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
+		mTitle = (String) getTitle();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerList = (ListView) findViewById(R.id.left_drawer);
+		menulists = new ArrayList<String>();
+		for (int i = 0; i < 5; i++)
+			menulists.add("小六" + i);
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, menulists);
+		mDrawerList.setAdapter(adapter);
+		mDrawerList.setOnItemClickListener(this);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+				R.drawable.ic_drawer, R.string.drawer_open,
+				R.string.drawer_close) {
+			@Override
+			public void onDrawerOpened(View drawerView) {
+				super.onDrawerOpened(drawerView);
+				getActionBar().setTitle("请选择");
+				invalidateOptionsMenu();
+
+			}
+
+			@Override
+			public void onDrawerClosed(View drawerView) {
+				super.onDrawerClosed(drawerView);
+				getActionBar().setTitle(mTitle);
+				invalidateOptionsMenu();
+			}
+		};
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+		
+		//在这里要ActionBar上 APP ICON的功能
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		boolean isDrawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+		menu.findItem(R.id.action_websearch).setVisible(!isDrawerOpen);
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		//将ActiongBar上的图标与Drawer结合起来
+		if(mDrawerToggle.onOptionsItemSelected(item)){
+			return true;
+		}
+		switch (item.getItemId()) {
+		case R.id.action_websearch:
+			Intent intent = new Intent();
+			intent.setAction("android.intent.action.VIEW");
+			Uri uri = Uri.parse("http://www.baidu.com");
+			intent.setData(uri);
+			startActivity(intent);
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+		
+		
+	}
+	
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		//需要将ActionDrawerToggle与DrawerLayout的状态同步
+		//将ActionBarDrawerToggle中的drawer图标，设置为ActionBar中的Home-Buutton的ICON
+		mDrawerToggle.syncState();
+	}
+	
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		mDrawerToggle.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		Fragment contentFragment = new ContentFragment();
+		Bundle args = new Bundle();
+		args.putString("text", menulists.get(position));
+		contentFragment.setArguments(args);
+
+		FragmentManager fm = getFragmentManager();
+		fm.beginTransaction().replace(R.id.content_frame, contentFragment)
+				.commit();
+
+		mDrawerLayout.closeDrawer(mDrawerList);
+
+	}
 }
